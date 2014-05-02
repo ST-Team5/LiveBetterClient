beaa38f67d0afd36f6bbf326218701765ff70fb0
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-02 11:12:46 +0300

    Added a scaffolded Angular application.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lbClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
