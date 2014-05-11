c79efd30ddece16b05dbc74b3c4690b0dcb2326b
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-11 15:21:41 +0300

    Removed the dummy view ("add-meal") from yesterday.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,10 +13,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/add-meal', {
-        templateUrl: 'views/add-meal.html',
-        controller: 'AddMealCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
