f5157942d968fcf47d213fcb5f84438f6f480555
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-18 11:58:32 +0300

    Tests for the main controller.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,16 +6,27 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('lbClientApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    location;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $location) {
+    location = $location;
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: scope,
     });
   }));
 
-  it('foo', function () {
+  it('properly detects the active route', function () {
+    location.path('/foo');
+    expect(scope.isActive('/foo')).toBeTruthy();
+    expect(scope.isActive('/baz')).toBeFalsy();
+    location.path('/baz');
+    expect(scope.isActive('/foo')).toBeFalsy();
+    expect(scope.isActive('/baz')).toBeTruthy();
+    location.path('/bar');
+    expect(scope.isActive('/foo')).toBeFalsy();
+    expect(scope.isActive('/baz')).toBeFalsy();
   });
 });
