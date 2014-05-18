7d74caa3c33db90f319c2299a5b65e63e24f914e
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-18 22:57:34 +0300

    Minor: Fixed an error in my last commit.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,27 @@
 'use strict';
 
-angular.module('lbClientApp', [
-  angular
-  .module('lbClientApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/add-new-activity-type', {
-        templateUrl: 'views/add-new-activity-type.html',
-        controller: 'ActivityCtrl'
-      })
-      .when('/add-food', {
-        templateUrl: 'views/add-food.html',
-        controller: 'AddFoodCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+angular
+.module('lbClientApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl'
   })
-]);
+  .when('/add-new-activity-type', {
+    templateUrl: 'views/add-new-activity-type.html',
+    controller: 'ActivityCtrl'
+  })
+  .when('/add-food', {
+    templateUrl: 'views/add-food.html',
+    controller: 'AddFoodCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
