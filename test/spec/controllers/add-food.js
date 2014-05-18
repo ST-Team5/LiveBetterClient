083cf230d91d7456e57e53e2793d1730c0d0d7c1
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-18 11:39:52 +0300

    Tests for the "add-food" controller.

--- test/spec/controllers/add-food.js
+++ test/spec/controllers/add-food.js
@@ -16,6 +16,55 @@ describe('Controller: AddFoodCtrl', function () {
     });
   }));
 
-  it('foo', function () {
+  it('returns the proper quantity label for foods', function () {
+    scope.item = {
+      type: 'food'
+    };
+    expect(scope.quantityLabel()).toBe('grams');
+  });
+
+  it('returns the proper quantity label for beverages', function () {
+    scope.item = {
+      type: 'beverage'
+    };
+    expect(scope.quantityLabel()).toBe('milliliters');
+  });
+
+  it('properly detects a food item', function () {
+    scope.item = {
+      type: 'food'
+    };
+    expect(scope.isFood()).toBeTruthy();
+    expect(scope.isBeverage()).toBeFalsy();
+  });
+
+  it('properly detects a beverage item', function () {
+    scope.item = {
+      type: 'beverage'
+    };
+    expect(scope.isFood()).toBeFalsy();
+    expect(scope.isBeverage()).toBeTruthy();
+  });
+
+  it('properly select a food item', function () {
+    scope.item = {
+      type: 'beverage'
+    };
+    expect(scope.isFood()).toBeFalsy();
+    expect(scope.isBeverage()).toBeTruthy();
+    scope.selectFood();
+    expect(scope.isFood()).toBeTruthy();
+    expect(scope.isBeverage()).toBeFalsy();
+  });
+
+  it('properly select a beverage item', function () {
+    scope.item = {
+      type: 'food'
+    };
+    expect(scope.isFood()).toBeTruthy();
+    expect(scope.isBeverage()).toBeFalsy();
+    scope.selectBeverage();
+    expect(scope.isFood()).toBeFalsy();
+    expect(scope.isBeverage()).toBeTruthy();
   });
 });
