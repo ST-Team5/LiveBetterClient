520839e0f181239240c9341039e0bcdeba6d9622
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-19 09:18:38 +0300

    UI fixes.

--- app/scripts/controllers/add-activity.js
+++ app/scripts/controllers/add-activity.js
@@ -1,18 +1,18 @@
 'use strict';
 
 angular.module('lbClientApp')
-.controller('ActivityCtrl', function($scope, $controller, ActivityService){
-	angular.extend(this, $controller('BaseCtrl', {$scope: $scope}));
+  .controller('ActivityCtrl', function ($scope, $controller, ActivityService) {
+    angular.extend(this, $controller('BaseCtrl', {$scope: $scope}));
 
-	$scope.insertActivity = function(){
-		var name = $scope.newActivity.name;
-		var type = $scope.newActivity.type;
-		var calories = $scope.newActivity.calories;
+    $scope.insertActivity = function () {
+      var name = $scope.newActivity.name;
+      var type = $scope.newActivity.type;
+      var calories = $scope.newActivity.calories;
 
-		ActivityService.insertActivity(name, type, calories);
+      ActivityService.insertActivity(name, type, calories);
 
-		$scope.newActivity.name = '';
-		$scope.newActivity.type = '';
-		$scope.newActivity.calories = '';
-	};
-});
+      $scope.newActivity.name = '';
+      $scope.newActivity.type = '';
+      $scope.newActivity.calories = '';
+    };
+  });
--- app/scripts/services/add-activity-service.js
+++ app/scripts/services/add-activity-service.js
@@ -1,32 +1,32 @@
 'use strict';
 
 angular.module('lbClientApp')
-	.service('ActivityService', function($http, SERVER_ADDRESS) {
-		
-	this.getAllActivities = function () {
-		
-	};
+  .service('ActivityService', function ($http, SERVER_ADDRESS) {
 
-	this.insertActivity = function(name, type, calories) {
+    this.getAllActivities = function () {
 
-		var activityToInsert = {
-			name: name,
-			type: type,
-			caloriesPerHour: calories
-		};
-		
-		$http({
-			method: 'POST',
-			url: SERVER_ADDRESS + 'activities/insert-new-type',
-			contentType: 'application/json',
-			data: JSON.stringify(activityToInsert)
-		}).
-		success(function(data) {
-			console.log(status, data);
-		}).
-		error(function(data) {
-		
-			console.log(data);
-		});
-	};
-});
+    };
+
+    this.insertActivity = function (name, type, calories) {
+
+      var activityToInsert = {
+        name: name,
+        type: type,
+        caloriesPerHour: calories
+      };
+
+      $http({
+        method: 'POST',
+        url: SERVER_ADDRESS + 'activities/insert-new-type',
+        contentType: 'application/json',
+        data: JSON.stringify(activityToInsert)
+      }).
+        success(function (data) {
+          console.log(status, data);
+        }).
+        error(function (data) {
+
+          console.log(data);
+        });
+    };
+  });
