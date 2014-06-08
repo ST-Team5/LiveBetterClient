b7dff9b68e92a1371bfaa9ea49af031280bfdeec
Georgi Hristozov <georgi@forkbomb.nl>
2014-06-08 23:05:19 +0300

    Support for getting all/recent/frequent items in the "select" views from different sources.

--- app/scripts/controllers/base-select-dialog-controller.js
+++ app/scripts/controllers/base-select-dialog-controller.js
@@ -1,19 +1,22 @@
 'use strict';
 
 angular.module('lbClientApp')
-  .controller('BaseSelectCtrl', function ($scope, $controller) {
-    angular.extend(this, $controller('BaseCtrl', {$scope: $scope}));
+  .controller('BaseSelectCtrl', function($scope, $controller) {
+    angular.extend(this, $controller('BaseCtrl', {
+      $scope: $scope
+    }));
 
-    this.initialize = function (contents) {
-      $scope.all = $scope.frequent = $scope.recent = _.map(
-        contents,
-        function (rawItem) {
-          var result = {
-            selected: false,
-            data: rawItem
-          };
-          return  result;
-        });
+    this.initialize = function(data) {
+      function transform(rawItem) {
+        var result = {
+          selected: false,
+          data: rawItem
+        };
+        return result;
+      }
+      $scope.all = _.map(data.all, transform);
+      $scope.recent = _.map(data.recent, transform);
+      $scope.frequent = _.map(data.frequent, transform);
     };
 
     $scope.isInputValid = false;
@@ -21,19 +24,19 @@ angular.module('lbClientApp')
     $scope.all = [];
     $scope.frequent = [];
 
-    $scope.frequentSelected = function () {
+    $scope.frequentSelected = function() {
       $scope.selectedProvider = $scope.frequent;
     };
 
-    $scope.recentSelected = function () {
+    $scope.recentSelected = function() {
       $scope.selectedProvider = $scope.recent;
     };
 
-    $scope.allSelected = function () {
+    $scope.allSelected = function() {
       $scope.selectedProvider = $scope.all;
     };
 
-    $scope.invalidateInput = function () {
+    $scope.invalidateInput = function() {
       var provider = $scope.selectedProvider;
       if (!provider) {
         $scope.isInputValid = false;
@@ -48,16 +51,16 @@ angular.module('lbClientApp')
       $scope.isInputValid = false;
     };
 
-    $scope.doneButtonHandler = function () {
+    $scope.doneButtonHandler = function() {
       $scope.send(
         _.chain($scope.selectedProvider)
-          .filter(function (item) {
-            return item.selected;
-          })
-          .map(function (item) {
-            return item.data;
-          })
-          .value());
+        .filter(function(item) {
+          return item.selected;
+        })
+        .map(function(item) {
+          return item.data;
+        })
+        .value());
     };
 
     $scope.$watch('selectedProvider', $scope.invalidateInput());
--- app/scripts/services/get-food-service.js
+++ app/scripts/services/get-food-service.js
@@ -4,9 +4,33 @@ angular.module('lbClientApp')
   .service('GetFoodService', function ($http, SERVER_ADDRESS, $q) {
     this.getAll = function() {
       var deferred = $q.defer();
+      var result = {};
+      var counter = 0;
+
+      function notifyIfReady() {
+        if (counter >= 3) {
+          deferred.resolve(result);
+        }
+      }
+
       $http.get(SERVER_ADDRESS + 'meals/list').success(function(data) {
-        return deferred.resolve(data);
+        counter++;
+        result.all = data;
+        notifyIfReady();
       });
+
+      $http.get(SERVER_ADDRESS + 'meals/list').success(function(data) {
+        counter++;
+        result.recent = data;
+        notifyIfReady();
+      });
+
+      $http.get(SERVER_ADDRESS + 'meals/list').success(function(data) {
+        counter++;
+        result.frequent = data;
+        notifyIfReady();
+      });
+
       return deferred.promise;
     };
   });
