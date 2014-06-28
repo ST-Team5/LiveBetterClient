9b3f108b4a5d8d6365216f442d7575bec59c9108
Georgi Hristozov <georgi@forkbomb.nl>
2014-06-28 16:24:55 +0300

    Removed the hardcoded data.

--- app/scripts/services/get-main-screen-data-service.js
+++ app/scripts/services/get-main-screen-data-service.js
@@ -1,22 +1,30 @@
 'use strict';
 
 angular.module('lbClientApp')
-  .service('GetMainScreenDataService', function ($http, SERVER_ADDRESS, $q) {
+  .service('GetMainScreenDataService', function ($http, SERVER_ADDRESS, $q, CURRENT_USER_ID) {
     this.getData = function (date) {
       var deferred = $q.defer();
 
       var actualDate = date ? date : new Date();
 
-      setTimeout(function () {
+      var url = SERVER_ADDRESS + 'mainScreen/' + CURRENT_USER_ID + '/' + actualDate.getFullYear() + '/' +
+        (actualDate.getMonth() + 1) + '/' + actualDate.getDate();
+      $http.get(url).success(function (data) {
+        data.a = true;
+        var consumedCalories = data.caloriesConsumed;
+        var burnedCalories = data.caloriesBurned;
+        var remainingCalories = data.caloriesRemaining;
+//        var progress = (burnedCalories / (burnedCalories + remainingCalories)) * 100;
+        var progress = (burnedCalories / 2000) * 100;
         deferred.resolve({
           timestamp: actualDate.getTime(),
-          caloriesConsumed: 1337,
-          caloriesBurned: 337,
-          caloriesRemaining: 1000,
-          minutesExercised: 66,
-          progress: 76
+          caloriesConsumed: consumedCalories,
+          caloriesBurned: burnedCalories,
+          caloriesRemaining: remainingCalories,
+          minutesExercised: data.minutesExercised,
+          progress: progress
         });
-      }, 1000);
+      });
 
       return deferred.promise;
     };
