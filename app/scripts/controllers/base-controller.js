53ac084e77b0223819f99b62093b9e4ca9443396
Georgi Hristozov <georgi@forkbomb.nl>
2014-06-29 18:59:22 +0300

    Fixes in the selection dropdown logic.

--- app/scripts/controllers/base-controller.js
+++ app/scripts/controllers/base-controller.js
@@ -2,7 +2,13 @@
 
 angular.module('lbClientApp')
   .controller('BaseCtrl', function ($scope, $location) {
-    $scope.isActive = function(route) {
-        return route === $location.path();
-      };
+    $scope.isActive = function (route) {
+      return route === $location.path();
+    };
+    $scope.showSelectionDropdown = function () {
+      $('#selectionDropdown').addClass('open');
+    };
+    $scope.hideSelectionDropdown = function () {
+      $('#selectionDropdown').removeClass('open');
+    };
   });
